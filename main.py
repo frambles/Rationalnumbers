4b3c6cf3682acc8c9e9fa70e15f83f1150799ed3
Jroach <jroach@thegraysonschool.org>
2023-11-07 15:10:03 -0500

    fractions can be used for math operations

--- main.py
+++ main.py
@@ -32,8 +32,13 @@ class Rational:
                 self.denom *= -1
 
     def __add__(self, other):
-        top = self.numer * other.denom + other.numer * self.denom
-        bottom = self.denom * other.denom
+        if type(other) is int:
+            other = Rational(other, 1)
+            top = self.numer * other.denom + other.numer * self.denom
+            bottom = self.denom * other.denom
+        else:
+            top = self.numer * other.denom + other.numer * self.denom
+            bottom = self.denom * other.denom
 
         return Rational(top, bottom)
 
@@ -44,13 +49,18 @@ class Rational:
         return self + -other
 
     def __mul__(self, other):
+        if type(other) is int:
+            other = Rational(other, 1)
         return Rational((self.numer * other.numer), (self.denom * other.denom))
 
     def __invert__(self):
         return Rational(self.denom, self.numer)
 
     def __truediv__(self, other):
-        return self * ~other
+        if type(other) is Rational:
+            return self * ~other
+        else:
+            return self * Rational(1, other)
 
     def __float__(self):
         return float(self.numer / self.denom)
@@ -61,11 +71,68 @@ class Rational:
     def __abs__(self):
         return Rational(abs(self.numer), self.denom)
 
+    def __radd__(other, self):
+        return other + self
+
+    def __rsub__(other, self):
+        return other - self
+
+    def __rmul__(other, self):
+        return other * self
+
+    def __rtruediv__(other, self):
+        return self * ~other
+
+    def __rpow__(self, base):
+        return (base ** (1 / self.denom)) ** self.numer
+
+    def __eq__(self, other):
+        if float(self) == float(other):
+            return bool(1)
+        else:
+            return bool(0)
+
+    def __ne__(self, other):
+        if float(self) != float(other):
+            return bool(1)
+        else:
+            return bool(0)
+
+    def __lt__(self, other):
+        if float(self) < float(other):
+            return bool(1)
+        else:
+            return bool(0)
+
+    def __gt__(self, other):
+        if float(self) > float(other):
+            return bool(1)
+        else:
+            return bool(0)
+
+    def __le__(self, other):
+        if float(self) <= float(other):
+            return bool(1)
+        else:
+            return bool(0)
+
+    def __ge__(self, other):
+        if float(self) >= float(other):
+            return bool(1)
+        else:
+            return bool(0)
+
+    def from_float(flo):
+        if type is float:
+            return
+        else:
+            pass
+
 
 def main():
-    myrat = Rational(3, 4)
-    yourrat = Rational(3, 5)
-    print(abs(myrat))
+    myrat = Rational(1, 2)
+    yourrat = Rational(2, 2)
+    print(myrat <= yourrat)
 
 
 if __name__ == '__main__':
